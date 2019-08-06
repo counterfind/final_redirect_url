b9378c46428df24045616355d4168b217db3c866
Nicolas Risso <ndrisso@gmail.com>
2019-08-06 19:21:30 -0300

    Return last valid URL and remove annoying warn

--- lib/final_redirect_url.rb
+++ lib/final_redirect_url.rb
@@ -8,7 +8,7 @@ module FinalRedirectUrl
     final_url = ''
     if is_valid_url?(url)
       begin
-        redirect_lookup_depth = options[:depth].to_i > 0 ? options[:depth].to_i : 10
+        redirect_lookup_depth = options[:depth].to_i > 0 ? options[:depth].to_i : 5
         response_uri = get_final_redirect_url(url, redirect_lookup_depth)
         final_url =  url_string_from_uri(response_uri)
       rescue Exception => ex
@@ -20,32 +20,31 @@ module FinalRedirectUrl
   end
 
   private
+
   def self.is_valid_url?(url)
     url.to_s =~ /\A#{URI::regexp(['http', 'https'])}\z/
   end
 
-  def self.get_final_redirect_url(url, limit = 10)
+  def self.get_final_redirect_url(url, limit = 5)
     return url if limit <= 0
+
     uri = URI.parse(url)
     response = ::Net::HTTP.get_response(uri)
-    if response.class == Net::HTTPOK
-      return uri
-    else
-      redirect_location = response['location']
-      location_uri = URI.parse(redirect_location)
-      if location_uri.host.nil?
-        redirect_location = uri.scheme + '://' + uri.host + redirect_location
-      end
-      warn "redirected to #{redirect_location}"
-      get_final_redirect_url(redirect_location, limit - 1)
+    return uri if response.class == Net::HTTPOK
+
+    redirect_location = response['location']
+    return uri unless redirect_location
+
+    location_uri = URI.parse(redirect_location)
+    if location_uri.host.nil?
+      redirect_location = uri.scheme + '://' + uri.host + redirect_location
     end
+    get_final_redirect_url(redirect_location, limit - 1)
   end
 
   def self.url_string_from_uri(uri)
     url_str = "#{uri.scheme}://#{uri.host}#{uri.request_uri}"
-    if uri.fragment
-      url_str = url_str + "##{uri.fragment}"
-    end
+    url_str += "##{uri.fragment}" if uri.fragment
     url_str
   end
 end
