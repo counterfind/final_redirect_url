b5b1ef8b1b92a6c79a1ac49536cdfd8ddd3c2751
Nicolas Risso <ndrisso@gmail.com>
2019-08-09 09:50:49 -0300

    Return partially calculated url if error

--- lib/final_redirect_url.rb
+++ lib/final_redirect_url.rb
@@ -4,34 +4,38 @@ require 'logger'
 
 module FinalRedirectUrl
 
-  def self.final_redirect_url(url, options={})
-    final_url = url
-    if is_valid_url?(url)
+  def self.final_redirect_url(url, options = {})
+    @final_url = url
+
+    if valid_url?(url)
       begin
-        redirect_lookup_depth = options[:depth].to_i > 0 ? options[:depth].to_i : 5
-        response_uri = get_final_redirect_url(url, redirect_lookup_depth)
-        final_url =  url_string_from_uri(response_uri)
+        redirect_lookup_depth = options[:depth].to_i.positive? ? options[:depth].to_i : 5
+        timeout = options[:timeout].to_i.positive? ? options[:timeout].to_i : 3
+
+        response_uri = get_final_redirect_url(url, redirect_lookup_depth, timeout)
+        @final_url = url_string_from_uri(response_uri)
       rescue Exception => ex
         logger = Logger.new(STDOUT)
         logger.error(ex.message)
       end
     end
-    final_url
+
+    @final_url
   end
 
   private
 
-  def self.is_valid_url?(url)
-    url.to_s =~ /\A#{URI::regexp(['http', 'https'])}\z/
+  def self.valid_url?(url)
+    url.to_s =~ /\A#{URI.regexp(%w[http https])}\z/
   end
 
-  def self.get_final_redirect_url(url, limit = 5)
+  def self.get_final_redirect_url(url, limit = 5, timeout = 3)
     return url if limit <= 0
 
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
-    http.read_timeout = 3
-    http.open_timeout = 3
+    http.read_timeout = timeout
+    http.open_timeout = timeout
     response = http.start do |http|
       http.get(uri.path)
     end
@@ -44,7 +48,8 @@ module FinalRedirectUrl
     if location_uri.host.nil?
       redirect_location = uri.scheme + '://' + uri.host + redirect_location
     end
-    get_final_redirect_url(redirect_location, limit - 1)
+    @final_url = redirect_location
+    get_final_redirect_url(redirect_location, limit - 1, timeout)
   end
 
   def self.url_string_from_uri(uri)
